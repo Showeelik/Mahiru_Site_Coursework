a339a597c9fe0a91d929c39be530a3bd424435a0
Showeelik <zeezxex2000@yandex.ru>
2024-11-09 21:28:40 +0300

    Alpha 0.1.7 Добавлена модель для recipients

--- recipients/models.py
+++ recipients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipient(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Получатель'
+        verbose_name_plural = 'Получатели'
+        ordering = ['id']
