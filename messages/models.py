7cc8a30833ba3b1880249891ce9fc66e3624225f
Showeelik <zeezxex2000@yandex.ru>
2024-11-09 21:23:42 +0300

    Alpha 0.1.5 Добавлена модель для messages

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    theme = models.CharField(max_length=100)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.theme
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+        ordering = ['id']
+
+    
